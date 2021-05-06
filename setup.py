31bd68a5a08d38b1535239a01266208f41bb0d61
m <m@ms-MacBook-Air.local>
2021-05-06 08:56:54 -0400

    Trimming imported files

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     url='https://github.com/allegheny-computer-science-203-s2021/PytestPlugin-Team7',
     author='Mai Nguyen, Adriana Solis, Kyrie Doniz, Kevin Lee, Adam Shinomiya',
     author_email='nguyendacm@allegheny.edu,your_email@somewhere.com....add more',
-    license='...',
+    license='MIT',
     py_modules=['plugin'],
     install_requires=['pytest'],
     entry_points={'pytest11': ['pytest-plugin = pytestplugin.plugin', ], },
