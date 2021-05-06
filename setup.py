6b9462af68cc2b0dda75a351d0002f1f65babd66
solisa986 <solisa@allegheny.edu>
2021-05-06 11:54:24 -0400

    error fix

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 """Set up file for pytest plugin"""
 #!/usr/bin/env python3
-import os.path
-from typing import Dict
 
-import setuptools
 from setuptools import setup
 
 
@@ -25,71 +22,4 @@ setup(
     py_modules=['plugin'],
     install_requires=['pytest'],
     entry_points={'pytest11': ['pytest-plugin = pytestplugin.plugin', ], },
-)
-
-def get_release_command_class() -> Dict[str, setuptools.Command]:
-    try:
-        from releasecmd import ReleaseCommand
-    except ImportError:
-        return {}
-
-    return {"release": ReleaseCommand}
-
-
-with open("README.rst", encoding=ENCODING) as f:
-    LONG_DESCRIPTION = f.read()
-
-with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
-    INSTALL_REQUIRES = [line.strip() for line in f if line.strip()]
-
-with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
-    TESTS_REQUIRES = [line.strip() for line in f if line.strip()]
-
-
-setuptools.setup(
-    name=MODULE_NAME,
-    version=pkg_info["__version__"],
-    url=REPOSITORY_URL,
-    author=pkg_info["__author__"],
-    author_email=pkg_info["__email__"],
-    description="A pytest plugin to make a test results report with Markdown table format.",
-    include_package_data=True,
-    keywords=["pytest", "plugin", "markdown"],
-    license=pkg_info["__license__"],
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/x-rst",
-    packages=setuptools.find_packages(),
-    package_data={MODULE_NAME: ["py.typed"]},
-    project_urls={
-        "Source": REPOSITORY_URL,
-        "Tracker": "{:s}/issues".format(REPOSITORY_URL),
-    },
-    python_requires=">=3.6",
-    install_requires=INSTALL_REQUIRES,
-    extras_require={"test": TESTS_REQUIRES},
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Plugins",
-        "Framework :: Pytest",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Testing",
-    ],
-    cmdclass=get_release_command_class(),
-    zip_safe=False,
-    entry_points={
-        "pytest11": [
-            "pytest-md-report = pytest_md_report.plugin",
-        ]
-    },
 )
