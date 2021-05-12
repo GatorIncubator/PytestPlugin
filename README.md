8004287a02d52c5f76963e667fae9cc58be90209
m <m@ms-MacBook-Air.local>
2021-05-11 23:47:51 -0400

    Update plugin-email with arhuments, dataframe, and run command in Readme

--- README.md
+++ README.md
@@ -11,4 +11,19 @@ A pytest plugin to make a test results report with Markdown table format and the
 Dependencies
 ============================================
 - Python 3.6+
-- `Python package dependencies (automatically installed)
+- `Python package dependencies (automatically installed)`
+
+## Email plugin: plugin-email
+
+- Run command:
+
+```
+pipenv run pytest --mail --emailfrom="test@gmail.com" --pwd="XXXXXX" --emailto="test1@gmail.com"
+
+```
+- Argument included:
+
+- `--mail`: Enable email plugin when called
+- `--emailfrom`: Store the email address of the sender
+- `--pwd`: Store the password of the sender's email
+- `--emailto`: Store the email address of the receiver
--- pytest_email/plugin_email.py
+++ pytest_email/plugin_email.py
@@ -1,7 +1,8 @@
 """Adding a pytest plugin that send report to email"""
 
-import smtplib
+import yagmail
 import pytest
+import pandas as pd
 from _pytest.terminal import TerminalReporter
 
 
@@ -12,14 +13,14 @@ def pytest_addoption(parser):
     group.addoption("--mail", action="store_true",
     help="mail: send test report to user's email")
 
-    group.addoption("--emailto", action="store_true",
-    help="email-to: enter the receiver email")
+    group.addoption("--emailto", action="store", dest = "emailto",
+    default=None, help="email-to: enter the receiver email")
 
-    group.addoption("--emailfrom", action="store_true",
-    help="email-to: enter your email")
+    group.addoption("--emailfrom", action="store", dest = "emailfrom",
+    default=None, help="email-to: enter your email")
 
-    group.addoption("--pwd", action="store_true",
-    help="--password: enter your email password")
+    group.addoption("--pwd", action="store", dest = "pwd",
+    default=None, help="--password: enter your email password")
 
 
 def pytest_terminal_summary(terminalreporter, config):
@@ -30,28 +31,24 @@ def pytest_terminal_summary(terminalreporter, config):
     body = {"Passed test": passed,
             "Failed test": failed,
             "Total test": total}
+    body_df = pd.DataFrame(list(body.items()),columns = ['Status','Amount of test'])
+
     if config.getoption("--mail") is True:
-        receiver = config.option.emailto
-        password = config.option.pwd
-        sender = config.option.emailfrom
-        yagmail_prompt(body, senders, password, receiver)
+        receiver = str(config.option.emailto)
+        pwd = str(config.option.pwd)
+        sender = str(config.option.emailfrom)
+        yagmail_prompt(body_df, sender, pwd, receiver)
 
 
-def gmail_prompt(body, sender, pwd, receiver):
+def yagmail_prompt(body, sender, pwd, receiver):
     """Set up the email content and address"""
-    server = smtplib.SMTP(smtp)
-
-    msg = MIMEMultipart()
-    msg['Subject'] = "This is the test stats of your latest pytest"
-    msg['From'] = sender
-    msg['To'] = receiver
-    msg.attach(MIMEText(body))
-
     try:
-        server.starttls()
-        if use_auth:
-            server.login(user, pwd)
-            server.sendmail(user, receivers, msg.as_string())
+        yag = yagmail.SMTP(user = sender, password = pwd)
+        yag.send(
+            to=receiver,
+            subject="Test stats of your latest pytest",
+            contents= body
+            )
         print("Email succeffully")
     except:
-        print("Error, can't send email")
+        print("Error, can't send email, try --help")
