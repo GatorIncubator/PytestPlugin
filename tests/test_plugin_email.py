ea2473e54f846c24323f39c9bfa0cd35e2446a49
TheShiny1 <shinomiyaa@allegheny.edu>
2021-05-15 15:12:39 -0400

    beginning of test cases

--- tests/test_plugin_email.py
+++ tests/test_plugin_email.py
@@ -1,7 +1,16 @@
 """Test cases for the added emailing feature"""
 
 import pytest
+import sys
 
 from pytestplugin import plugin
 
 # add the test cases for the email feature to this file with the appropiate comment
+def test_pytest_email_terminal_summary():
+    """testing the output of email terminal summary""" # place holder
+
+def test_yagmail_prompt():
+    """testing the set up of the email content and address"""
+
+def test_pytest_addoption():
+    """testing the usage of the email"""
