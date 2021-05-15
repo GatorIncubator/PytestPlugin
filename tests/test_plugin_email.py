3119f39bea264d6144727ad904cc9bd166b14ba1
TheShiny1 <shinomiyaa@allegheny.edu>
2021-05-15 15:13:30 -0400

    changed the position of test case for email function

--- tests/test_plugin_email.py
+++ tests/test_plugin_email.py
@@ -6,11 +6,11 @@ import sys
 from pytestplugin import plugin
 
 # add the test cases for the email feature to this file with the appropiate comment
+def test_pytest_addoption():
+    """testing the usage of the email"""
+
 def test_pytest_email_terminal_summary():
     """testing the output of email terminal summary""" # place holder
 
 def test_yagmail_prompt():
     """testing the set up of the email content and address"""
-
-def test_pytest_addoption():
-    """testing the usage of the email"""
