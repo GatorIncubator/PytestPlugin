30f4646679c49895dba16d4b25b8ad27846e6fd6
TheShiny1 <shinomiyaa@allegheny.edu>
2021-05-20 17:48:20 -0400

    test

--- tests/test_plugin_email.py
+++ tests/test_plugin_email.py
@@ -6,11 +6,8 @@ import sys
 from pytest_email import plugin_email
 
 # add the test cases for the email feature to this file with the appropiate comment
-def test_pytest_addoption():
-    """testing the usage of the email addoption functions"""
-
 def test_pytest_email_terminal_summary():
     """testing the output of email terminal summary""" # place holder
-
+    
 def test_yagmail_prompt():
     """testing the set up of the email content and address"""
